ab0da51e3cd8d3a5f4049a844eef62da18a3ea9f
Hitendra <mahajan.hitendra@gmail.com>
2025-05-21 02:26:11 +0530

    new changes

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BloodDonation.settings')
     try:
         from django.core.management import execute_from_command_line
-        print('hai')
+        print('hi')
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
