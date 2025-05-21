7b3182f8592c46fc7584fed2a048ec25090443d1
Hitendra <mahajan.hitendra@gmail.com>
2025-05-21 10:39:26 +0530

    new changes

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BloodDonation.settings')
-    try:
+    try:n okfvgiobmgoi
         from django.core.management import execute_from_command_line
         print('hffffihhhhhkogkornkfbnpmbpotemhqpo')
     except ImportError as exc:
