1051480f24b09fd60b680393edcb5929ae49520f
Hitendra <mahajan.hitendra@gmail.com>
2025-05-21 10:42:33 +0530

    new changes

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def main():
         print('hffffihhhhhkogkornkfbnpmbpotemhqpo')
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+            "Couldn't import Django.nbobnorhmio Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
